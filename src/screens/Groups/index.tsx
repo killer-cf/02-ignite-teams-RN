5575ed0a2ff2a57aa3802e607b869f62df07368f
killer-cf <costa.kilder@live.com>
2023-03-20 11:32:29 -0300

    style: Header

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,9 +1,10 @@
+import { Header } from '@components/Header';
 import * as S from './styles';
 
 export function Groups() {
   return (
     <S.Container>
-      <S.Text>Groups</S.Text>
+      <Header />
     </S.Container>
   );
 }
--- src/screens/Groups/styles.ts
+++ src/screens/Groups/styles.ts
@@ -4,8 +4,7 @@ export const Container = styled.View`
   flex: 1;
   background-color: ${({ theme })=> theme.COLORS.GRAY_600};
   align-items: center;
-  justify-content: center;
-
+  padding: 35px 20px;
 `
 
 export const Text = styled.Text`
