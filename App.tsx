5694eb9c09be16c5c3e58f814643ef9a0786861d
killer-cf <costa.kilder@live.com>
2023-03-20 11:01:56 -0300

    style: Loading

--- App.tsx
+++ App.tsx
@@ -2,10 +2,15 @@ import { Groups } from '@screens/Groups';
 import theme from './src/theme';
 import { ThemeProvider } from 'styled-components';
 
+import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto'
+import { Loading } from '@components/Loading';
+
 export default function App() {
+  const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold})
+
   return (
     <ThemeProvider theme={theme}>
-      <Groups />
+      { fontsLoaded ? <Groups /> : <Loading /> }
     </ThemeProvider>
   );
 }
