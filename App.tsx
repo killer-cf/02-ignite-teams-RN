94536ca149fe0850c8959d76f655c3a2cf166fed
killer-cf <costa.kilder@live.com>
2023-03-20 11:05:27 -0300

    style: StatusBar

--- App.tsx
+++ App.tsx
@@ -1,6 +1,7 @@
 import { Groups } from '@screens/Groups';
 import theme from './src/theme';
 import { ThemeProvider } from 'styled-components';
+import { StatusBar  } from 'react-native';
 
 import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto'
 import { Loading } from '@components/Loading';
@@ -10,6 +11,11 @@ export default function App() {
 
   return (
     <ThemeProvider theme={theme}>
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      />
       { fontsLoaded ? <Groups /> : <Loading /> }
     </ThemeProvider>
   );
