118153a970f35050c2e1b2d225d9978841ff7b66
killer-cf <costa.kilder@live.com>
2023-03-20 18:04:23 -0300

    style: Filter

--- src/screens/Players/index.tsx
+++ src/screens/Players/index.tsx
@@ -1,11 +1,17 @@
 import { ButtonIcon } from "@components/ButtonIcon";
+import { Filter } from "@components/Filter";
 import { Header } from "@components/Header";
 import { Highlight } from "@components/Highlight";
 import { Input } from "@components/Input";
+import { useState } from "react";
+import { FlatList } from "react-native";
 
-import { Container, Form } from "./styles";
+import { Container, Form, HeaderList, NumberOfPlayers } from "./styles";
 
 export function Players() {
+  const [team, setTeam] = useState('time a')
+  const [players, setPlayers] = useState(['kilder', 'let'])
+
   return (
     <Container>
       <Header showBackButton />
@@ -22,6 +28,27 @@ export function Players() {
         />
         <ButtonIcon icon="add"/>
       </Form>
+
+      <HeaderList>
+        <FlatList
+        data={['time a', 'time b']}
+        keyExtractor={item => item}
+        renderItem={({item})=> (
+          <Filter 
+            title={item}
+            isActive={ item === team }
+            onPress={() => setTeam(item)}
+          /> 
+        )}
+        horizontal
+
+        />
+
+        <NumberOfPlayers>
+          {players.length}
+        </NumberOfPlayers>
+
+      </HeaderList>   
     </Container>
   )
 }
